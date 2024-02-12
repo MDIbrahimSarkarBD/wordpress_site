e34ce300baf45049f55ddf8f81e893637b8367c1
MD Ibrahim sarkar BD <ibrahimsarkar799@gmail.com>
2024-02-13 00:22:26 +0600

    theme  fundamental stady

--- wp-content/themes/mythemes/functions.php
+++ wp-content/themes/mythemes/functions.php
@@ -1,21 +1,37 @@
 <?php
-add_action( 'after_setup_theme', 'theme_slug_setup' );
+//add_action( 'after_setup_theme', 'theme_slug_setup' );
 
-function theme_slug_setup() {
-	add_theme_support( 'wp-block-styles' );
-}
+// function theme_slug_setup() {
+// 	add_theme_support( 'wp-block-styles' );
+// }
 
 
 
 add_action( 'wp_enqueue_scripts', 'is_enqueue_styles' );
 
 function is_enqueue_styles()  {
-    wp_enqueue_style("is_main_css", get_stylesheet_uri());
+   // wp_enqueue_style("is_main_css", get_stylesheet_uri());
 
-    wp_add_inline_style( 
-		'theme-slug-primary', 
-		'body { background: red; }'
-	);
+   // custom add css
+  wp_enqueue_style('is_bosy_css', get_parent_theme_file_uri( '/assets/css/body.css' ) ,array() , '1.0.0', 'all');
+  wp_add_inline_style( 'is_bosy_css', 'body { background: #895; }');
 
-    wp_enqueue_script('is_main_script', get_theme_file_uri( 'assets/js/main.js' ), array(), '1.0.0' );
+  wp_enqueue_script('is_main_script', get_theme_file_uri( 'assets/js/main.js?t='. time()), array(), '1.0.0' );
 }
+
+// Afet setup theme thumbnails suppots
+add_action('after_setup_theme', 'is_theme_customize');
+
+if (!function_exists('is_theme_customize')) {
+    
+    function is_theme_customize(){
+
+        // Afet setup theme thumbnails suppots
+        //add_theme_support('post-thumbnails');
+
+        add_theme_support('post-thumbnails', array('post', 'service','page'));
+
+    }
+}
+
+
--- wp-content/themes/mythemes/index.php
+++ wp-content/themes/mythemes/index.php
@@ -3,12 +3,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MY First php</title>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <title><?php wp_title('|', true, "right")?></title>
+    <?php wp_head();
+          
+    ?>
 </head>
 <body>
-<?php echo get_stylesheet_directory_uri(); ?>
 <h1>Ibrahim sarkar</h1>
+<?php get_parent_theme_file_uri( '/parts/header.php' ); 
+?>
+<!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:site-title {"level":0} /-->
+
+	<!-- wp:paragraph -->
+		<p>Powered by WordPress.</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+
+<?php wp_footer(); ?>
 </body>
 </html>
